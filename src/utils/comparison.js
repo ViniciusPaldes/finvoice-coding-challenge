76caf6319a8e2d6b059314f7aef2a126561f32a4
Vinicius Dias Paldes <vinnydp@gmail.com>
2021-01-28 18:56:36 -0300

    fix: Improved the comparison to avoid non-match by position diff

--- src/utils/comparison.js
+++ src/utils/comparison.js
@@ -1,15 +1,25 @@
 import _isInteger from 'lodash/isInteger';
+import _map from 'lodash/map';
+import _filter from 'lodash/filter';
 
-const iterateObj = (data, singleLevelArray) => {
-    for(var key in data){
-        if (!_isInteger(parseInt(key))) {
-            if (!(typeof data[key] === 'object' && data[key] !== null)) {
-                singleLevelArray.push({key, data: data[key]});
-            }
-        }
+const transformJsonIntoSingleLevel = (data, preset, singleLevelArray) => {
+    for(let key in data){
+        let updatedPreset = preset;
         if (data.hasOwnProperty(key)) {
             if (data[key] !== null && typeof data[key] === 'object') {
-                data[key] = iterateObj(data[key], singleLevelArray);
+                if (_isInteger(parseInt(key))) {
+                    updatedPreset = updatedPreset?.length > 0 ? updatedPreset + '-' + 'x' : 'x';    
+                } else {
+                    updatedPreset = updatedPreset?.length > 0 ? updatedPreset + '-' + key : key;
+                }
+                data[key] = transformJsonIntoSingleLevel(data[key], updatedPreset, singleLevelArray);
+            } else {
+                if (updatedPreset) {
+                    updatedPreset = updatedPreset + '-' + key;
+                } else {
+                    updatedPreset = key;
+                }
+                singleLevelArray.push({key: updatedPreset, data: data[key]});
             }
         }
     }
@@ -25,18 +35,18 @@ export const compareJSON = (firstJson, secondJson) => {
     const firstObjSingleLevelArray = [];
     const secondObjSingleLevelArray = [];
 
-    iterateObj(firstJson, firstObjSingleLevelArray);
-    iterateObj(secondJson, secondObjSingleLevelArray);
+    transformJsonIntoSingleLevel(firstJson, '', firstObjSingleLevelArray);
+    transformJsonIntoSingleLevel(secondJson, '', secondObjSingleLevelArray);
 
     const total = Math.max(firstObjSingleLevelArray.length, secondObjSingleLevelArray.length);
     let equalCount = 0;
 
-    for (let i = 0; i < total; i++) {
-        if ( !!(firstObjSingleLevelArray[i]?.key === secondObjSingleLevelArray[i]?.key) && 
-             !!(firstObjSingleLevelArray[i]?.data === secondObjSingleLevelArray[i]?.data) ) {
-                equalCount++;
+    _map(firstObjSingleLevelArray, fJson => {
+        const similar = _filter(secondObjSingleLevelArray, sJson => sJson.data === fJson.data && sJson.key === fJson.key);
+        if (similar.length > 0) {
+            equalCount++;
         }
-    }
+    });
     
     return equalCount / total;
 }
